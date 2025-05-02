da4b94467f636a0f0efed1310ebe6226be291e1f
Brett Commandeur <bcommandeur@gmail.com>
2025-05-01 18:58:09 -0600

    Update SoundCloudFrame.tsx

--- app/components/SoundCloudFrame.tsx
+++ app/components/SoundCloudFrame.tsx
@@ -4,9 +4,9 @@ interface SoundCloudSrc {
 
 const SoundCloudFrame = (props: SoundCloudSrc) => {
     return (
-        <>
+        <div>
             <iframe className="h-96 flex-shrink-1 max-w-xl rounded-xl shadow" width="100%" height="100%" allow="autoplay" src={props.embed}></iframe>
-        </>
+        </div>
     ) 
 };
 
